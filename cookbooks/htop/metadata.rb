3cb7801ef6e0fa33069f4c146d87e7646d2576b0
Sinister Light <me@SinisterLight.com>
2013-06-07 17:42:50 +0530

    [Shobhit] updating htop

--- cookbooks/htop/metadata.rb
+++ cookbooks/htop/metadata.rb
@@ -4,4 +4,3 @@ license          "All rights reserved"
 description      "Installs/Configures htop"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
-depends          "epel"
