defb2ce00705f6e1e43b1abadece76b4cd64c3d7
Sinister Light <me@SinisterLight.com>
2013-06-10 18:56:49 +0530

    [Shobhit] removing trash from htop cookbook

--- cookbooks/htop/metadata.rb
+++ cookbooks/htop/metadata.rb
@@ -1,5 +1,5 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Sinister Light"
+maintainer_email "me@sinisterlight.com"
 license          "All rights reserved"
 description      "Installs/Configures htop"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
--- cookbooks/htop/recipes/default.rb
+++ cookbooks/htop/recipes/default.rb
@@ -1,10 +1,1 @@
-#
-# Cookbook Name:: htop
-# Recipe:: default
-#
-# Copyright 2012, y2cf digital media pvt ltd
-#
-# All rights reserved - Do Not Redistribute
-#
 package 'htop'
-
